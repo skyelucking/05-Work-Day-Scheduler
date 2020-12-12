e52bd83b756db3355b4b47114066088a5471b27a
Skye Lucking <lucking.skye@gmail.com>
2020-12-12 09:58:39 -0700

    cleaned up the code and added comments

--- script.js
+++ script.js
@@ -35,19 +35,14 @@ var timeBlocksObj = {
   17: {
     displayHour: "5 pm",
     reminder: "",
-  }
+  },
 };
 
 //Declare Date and Time Variables//
 var now = moment();
-console.log(now);
 var currentHeaderDate = moment().format("dddd, MMMM Do");
-console.log("Current date: " + currentHeaderDate);
 var currentTime = moment().format("hh:mm A");
-console.log("Current timee: " + currentTime);
 var currentHour = moment().format("H");
-console.log("Current hour: " + currentHour);
-console.log(timeBlockContainer);
 
 //Get Current Day and TIme
 function getCurrentDay() {
@@ -57,31 +52,25 @@ function getCurrentTime() {
   $("#currentTime").text(currentTime);
 }
 {
-  
 }
-
 //Render Time Blocks
 function renderTimeBlocks() {
   var timeBlockContainer = $("#timeBlockContainer");
   var row, col1, col2, col3, textArea, i;
- 
   for (const [key, value] of Object.entries(timeBlocksObj)) {
- 
     row = $("<div></div>").addClass("row time-block");
     col1 = $("<div></div>").addClass("col-1 hour").text(value.displayHour);
-    console.log([key] )
-    console.log([key] - currentHour)
     row.append(col1);
-    col2 = $("<div></div>")
-    .attr("id", [key]);
-    if ([key] ==+ currentHour) {
-        col2.addClass("col-10 description present")
+    //Formats Timeblocks for Past, Present, and Future
+    col2 = $("<div></div>").attr("id", [key]);
+    if ([key] == +currentHour) {
+      col2.addClass("col-10 description present");
     } else if ([key] > currentHour) {
-        col2.addClass("col-10 description past")
+      col2.addClass("col-10 description past");
     } else if ([key] < currentHour) {
-        col2.addClass("col-10 description future")
+      col2.addClass("col-10 description future");
     }
-      textArea = $("<textarea><textarea>")
+    textArea = $("<textarea><textarea>")
       .attr("id", "reminder" + key)
       .addClass("description")
       .val(value.reminder);
@@ -92,7 +81,6 @@ function renderTimeBlocks() {
       .attr("id", "saveBtn" + key);
     i = $("<i></i>").addClass("fas fa-pencil-alt").data("hour", key);
     i.data("reminder-id", "reminder" + key);
-    //     <i data-hour='9' data-reminder-id='reminder9'>
     i.on("click", function (e) {
       saveReminder(e);
     });
@@ -101,27 +89,22 @@ function renderTimeBlocks() {
     timeBlockContainer.append(row);
   }
 }
-// Format Time Blocks to Past, Present, Future
-function formatTimeBlocks() {
-    console.log(timeBlockContainer)
-    console.log("current hour2 " + currentHour)
-}
 
 function saveToLocalStorage() {
   localStorage.setItem("timeblocks", JSON.stringify(timeBlocksObj));
 }
 
 function getFromLocalStorage() {
-  // Check if local storage (LS) key exists
+// Check if local storage (LS) key exists
   if (localStorage.getItem("timeblocks")) {
-    // Then retrieve the associated value from LS
+// Then retrieve the associated value from LS
     timeBlocksObj = JSON.parse(localStorage.getItem("timeblocks"));
   }
 }
 
 // Set Reminders for Button Clicks
 function saveReminder(e) {
-  //update Reminder for hour
+//update Reminder for hour
   var hour = $(e.target).data("hour");
   var reminderId = $(e.target).data("reminder-id");
   var reminderText = $("#" + reminderId).val();
@@ -129,13 +112,11 @@ function saveReminder(e) {
   //Save to local storage
   saveToLocalStorage();
 }
-
+//Initializes Page
 function initialize() {
   getFromLocalStorage();
   renderTimeBlocks();
-  formatTimeBlocks();
-}
-
+  }
 $(document).ready(function () {
   getCurrentDay();
   getCurrentTime();
